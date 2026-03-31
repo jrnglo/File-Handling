a6890aa8786c2caea296c418c78c481feac16aa6
jarur <jarur@DESKTOP-2U3E2U8>
2026-03-31 14:29:08 +0800

    Add project files.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("****************************");
+        Console.WriteLine("First Name       | Last Name     |");
+        string filepath = "example.txt";
+        Console.Write("Enter First Name : ");
+        string name = Console.ReadLine();
+        //Write content to file 
+        File.WriteAllText(filepath, name);
+        Console.Write("File written successfuly");
+
+        //Read content from file 
+        if (File.Exists(filepath))
+            {
+            string content = File.ReadAllText(filepath);
+            Console.WriteLine(content);
+        }
+        else
+        {
+            Console.WriteLine("File does not exist.");
+        }
+    }
+}
